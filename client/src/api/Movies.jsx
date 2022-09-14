ab4d840f52d1b0bf6f6421c135c619444c57f10e
Alyzen Jeraj <alyzenjeraj@gmail.com>
2022-09-14 17:06:35 -0400

    ready for production

--- client/src/api/Movies.jsx
+++ client/src/api/Movies.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
-
+const baseURL = process.env.NODE_ENV === 'production' ? '/movies' : 'http://localhost:5000/movies';
 export default axios.create({
-    baseURL: 'http://localhost:5000/movies'
+     baseURL
 });
 
